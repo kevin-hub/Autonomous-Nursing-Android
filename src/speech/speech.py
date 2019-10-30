7322ad831f4c5a98612ba0b6c8c5de62505a9ab6
Mayank <mayanksurana601@gmail.com>
2019-10-30 19:47:59 +0000

    started working on the speech processing

---
+++ src/speech/speech.py
@@ -0,0 +1,6 @@
+# Ros_speech_recognition
+
+# https://index.ros.org/p/ros_speech_recognition/github-jsk-ros-pkg-jsk_3rdparty/#melodic
+
+# http://wiki.ros.org/pocketsphinx
+
---
+++ src/speech/speech_to_text.py
@@ -0,0 +1,7 @@
+#tts ROS node enables a robot to speak with a human voice by providing a Text-To-Speech service
+
+#This will form the out-going dependency on the robot
+
+# https://index.ros.org/p/tts/github-aws-robotics-tts-ros1/#melodic 
+
+# http://wiki.ros.org/sound_play
