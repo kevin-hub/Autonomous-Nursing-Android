510e72a353547360a70634df4e646e2e72416dd0
prl4 <MayankSur>
2019-11-11 20:21:37 +0000

    Cleaned the files

--- setup.sh
+++ setup.sh
@@ -12,14 +12,14 @@ catkin_make
 BASEDIR=$(dirname "$0")
 echo "Current Directory = $BASEDIR"
 
-echo “source $BASEDIR/devel/setup.bash” >> ~/.bashrc
-source ~/.bashrc
+# echo “source $BASEDIR/devel/setup.bash” >> ~/.bashrc
+# source ~/.bashrc
 
 # Speech Dependencies
-sudo apt-get install python3 python3-all-dev python3-pip build-essential swig git libpulse-dev libasound2-dev portaudio19-dev python3-pyaudio -y
+sudo apt-get install python python-all-dev python-pip build-essential swig git libpulse-dev libasound2-dev portaudio19-dev python3-pyaudio -y
 #etc.
 
-pip3 install catkin_pkg
+pip install -r requirements.txt
 
 sudo apt-get install ros-kinetic-sound-play -y
 
