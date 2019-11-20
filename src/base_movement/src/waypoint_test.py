43432917c41239daca38fa3a794471f56eb00ee1
prl1 <mayanksurana601@gmail.com>
2019-11-20 15:13:59 +0000

    waypoint function using odometry working

--- src/base_movement/src/waypoint_test.py
+++ src/base_movement/src/waypoint_test.py
@@ -1,23 +1,51 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import PoseStamped
+import tf 
+
+
 
 
 def make_waypoint_pose(x,y,theta):
-	pose = PoseStamped()
-	pose.header.stamp = rospy.Time.now()
-	pose.header.frame_id = "map"
-	pose.pose.position.x = x
-	pose.pose.position.y = y
-	pose.pose.position.z = 
-
-	quaternion = tf.transformations.quaternion_from_euler(0, 0, theta)
-	pose.pose.orientation.x = quaternion[0]
-	pose.pose.orientation.y = quaternion[1]
-	pose.pose.orientation.z = quaternion[2]
-	pose.pose.orientation.w = quaternion[3]
+	
 	return pose()
 
-x = 0
-y = 0
-theta = 0
-point = make_waypoint_pose(x,y,theta)
+def publish_waypoint():
+    rospy.init_node('wp_publisher')
+
+    x = 0
+    y = 0
+    theta = 3.14
+    pose = PoseStamped()
+    pose.header.seq = 1
+    pose.header.stamp = rospy.Time.now()
+    pose.header.frame_id = "odom"
+    pose.pose.position.x = x
+    pose.pose.position.y = y
+    pose.pose.position.z = 0
+
+    quaternion = tf.transformations.quaternion_from_euler(0, 0, theta)
+    pose.pose.orientation.x = quaternion[0]
+    pose.pose.orientation.y = quaternion[1]
+    pose.pose.orientation.z = quaternion[2]
+    pose.pose.orientation.w = quaternion[3]
+
+    pub = rospy.Publisher('/move_base_simple/goal',PoseStamped,queue_size=10)
+
+    #while not rospy.is_shutdown():
+    rospy.loginfo(pose)
+    rospy.Rate(5).sleep()
+    pub.publish(pose)
+
+
+if __name__=='__main__':
+    try:
+        publish_waypoint()
+        
+    except rospy.ROSInterruptException:
+        pass
+
+
+
 
 
